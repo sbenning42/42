3a5ddc4b6a5a4e0e62d9080bfb5652df3debff87
Samuel BENNING <sbenning@e2r5p13.42.fr>
2015-12-02 12:02:56 +0100

    master add script

--- proj/fdf/srcs/fdf_exec.c
+++ proj/fdf/srcs/fdf_exec.c
@@ -6,7 +6,7 @@
 /*   By: sbenning <sbenning@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 16:54:03 by sbenning          #+#    #+#             */
-/*   Updated: 2015/12/02 00:29:23 by sbenning         ###   ########.fr       */
+/*   Updated: 2015/12/02 10:31:26 by sbenning         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 void		fdf_exec(t_list *lst, int ac, char **av)
 {
 	char	title[1024];
-	int		son_pid[ac];
+	int		son_pid;
 	int		i;
 	i = 0;
 	
-	while (++i < ac && lst)
+	while (++i < ac || lst)
 	{
-		ft_snprintf(title, 1024, "FDF: %s", av[i]);
-		son_pid[i] = fork();
-		if (!son_pid[i])
+		ft_snprintf(title, 1024, "FDF: %s", ac != 1 ? av[i] : "Standrd input");
+		son_pid = fork();
+		if (!son_pid)
 			fdf_mlx(*((t_list **)lst->content), title);
 		else
 		{
-			if (son_pid[i] < 0)
+			if (son_pid < 0)
 				ft_printf("fdf: {red}Error: %s fork{eoc}\n");
 			lst = lst->next;
 		}
