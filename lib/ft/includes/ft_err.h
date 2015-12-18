bab27f41614dfe7ea4cd3c16e7490e7172ca6611
m4s <m4s@debian>
2015-12-18 10:14:42 +0100

    master add err to libft

--- lib/ft/includes/ft_err.h
+++ lib/ft/includes/ft_err.h
@@ -1,29 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_err.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbenning <sbenning@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/10/25 17:59:34 by sbenning          #+#    #+#             */
-/*   Updated: 2015/12/18 10:12:36 by sbenning         ###   ########.fr       */
+/*   Created: 2015/12/18 10:10:14 by sbenning          #+#    #+#             */
+/*   Updated: 2015/12/18 10:12:39 by sbenning         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef FT_ERR_H
+# define FT_ERR_H
 
+# include <errno.h>
 # include "ft_header.h"
-# include "ft_char.h"
-# include "ft_str.h"
-# include "ft_wchar.h"
-# include "ft_lst.h"
-# include "ft_dar.h"
-# include "ft_dar2.h"
-# include "ft_buff.h"
-# include "ft_mem.h"
-# include "ft_put.h"
-# include "ft_err.h"
 # include "ft_printf.h"
 
 #endif
